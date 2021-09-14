597e1d39d55d6dcbada50db6bf21eb2ff02da046
Dipta437 <53457948+Dipta437@users.noreply.github.com>
2021-09-14 12:28:51 +0600

    added function

---
+++ js/pract.js
@@ -0,0 +1,66 @@
+const button1 = document.getElementById("firstRam");
+const button2 = document.getElementById("secondRam");
+const button3 = document.getElementById("firstStorage");
+const button4 = document.getElementById("secondStorage");
+const button5 = document.getElementById("thirdStorage");
+const button6 = document.getElementById("firstShippingCharge");
+const button7 = document.getElementById("secondShippingCharge");
+
+const total = document.getElementById("baseTotal");
+total.innerText = "$1299";
+
+
+const additionalPrice = document.getElementById("additionalPrice");
+const additionalStoragePrice = document.getElementById("additionalStoragePrice");
+const shippingCost = document.getElementById("shippingCost");
+
+
+button1.addEventListener("click", function(){
+    firstRamPrice.innerText = "$80";
+    updateTotal();
+});
+button2.addEventListener("click", function(){
+    firstRamPrice.innerText = "$160";
+    updateTotal();
+});
+button3.addEventListener("click", function(){
+    additionalStoragePrice.innerText = "$100";
+    updateTotal();
+});
+button4.addEventListener("click", function(){
+    additionalStoragePrice.innerText = "$120";
+    updateTotal();
+})
+button5.addEventListener("click", function(){
+    additionalStoragePrice.innerText = "$130";
+    updateTotal();
+})
+button6.addEventListener("click", function(){
+    shippingCost.innerText = "$0";
+    updateTotal();
+})
+button7.addEventListener("click", function(){
+    shippingCost.innerText = "$20";
+    updateTotal();
+})
+
+    
+
+// update total 
+    function updateTotal(){
+        const ramPrice = parseInt(firstRamPrice.innerText);
+        const storagePrice = parseInt(additionalStoragePrice.innerText);
+        const shipping = parseInt(shippingCost.innerText);
+        const cost = parseInt(total.innerText);
+
+        const grandTotal = parseInt(ramPrice + storagePrice + shipping + cost);
+        total.innerText = grandTotal;
+        
+       
+        
+        
+
+    
+        
+        
+    }
