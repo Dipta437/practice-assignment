a9553e00998604c359eb651ebff7cbf79b3b1be9
Dipta437 <53457948+Dipta437@users.noreply.github.com>
2021-09-21 02:12:35 +0600

    added update total

--- js/pract.js
+++ js/pract.js
@@ -6,8 +6,14 @@ const button5 = document.getElementById("thirdStorage");
 const button6 = document.getElementById("firstShippingCharge");
 const button7 = document.getElementById("secondShippingCharge");
 
-const total = document.getElementById("baseTotal");
-total.innerText = "$1299";
+const total = document.getElementById("fixedPrice");
+total.innerText = "1299";
+
+const grandTotal = document.getElementById("baseTotal");
+grandTotal.innerText = "1299";
+
+const finalTotal = document.getElementById("finalTotal");
+finalTotal.innerText = "1299";
 
 
 const additionalPrice = document.getElementById("additionalPrice");
@@ -16,31 +22,31 @@ const shippingCost = document.getElementById("shippingCost");
 
 
 button1.addEventListener("click", function(){
-    firstRamPrice.innerText = "$80";
+    firstRamPrice.innerText = "80";
     updateTotal();
 });
 button2.addEventListener("click", function(){
-    firstRamPrice.innerText = "$160";
+    firstRamPrice.innerText = "160";
     updateTotal();
 });
 button3.addEventListener("click", function(){
-    additionalStoragePrice.innerText = "$100";
+    additionalStoragePrice.innerText = "100";
     updateTotal();
 });
 button4.addEventListener("click", function(){
-    additionalStoragePrice.innerText = "$120";
+    additionalStoragePrice.innerText = "120";
     updateTotal();
 })
 button5.addEventListener("click", function(){
-    additionalStoragePrice.innerText = "$130";
+    additionalStoragePrice.innerText = "130";
     updateTotal();
 })
 button6.addEventListener("click", function(){
-    shippingCost.innerText = "$0";
+    shippingCost.innerText = "0";
     updateTotal();
 })
 button7.addEventListener("click", function(){
-    shippingCost.innerText = "$20";
+    shippingCost.innerText = "20";
     updateTotal();
 })
 
@@ -52,9 +58,15 @@ button7.addEventListener("click", function(){
         const storagePrice = parseInt(additionalStoragePrice.innerText);
         const shipping = parseInt(shippingCost.innerText);
         const cost = parseInt(total.innerText);
+        
+        const totalPrice = ramPrice + storagePrice + shipping + cost;
 
-        const grandTotal = parseInt(ramPrice + storagePrice + shipping + cost);
-        total.innerText = grandTotal;
+        grandTotal.innerText = totalPrice;
+
+        finalTotal.innerText = totalPrice;
+        
+    
+        
         
        
         
